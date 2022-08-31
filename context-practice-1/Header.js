055313725ca131c17ec98fad695cfd2b2d7ec1ee
ycarpenter <yurii.carpenter@gmail.com>
2022-08-31 19:24:43 +0300

    feat(header): implement dynamic change user name

--- context-practice-1/Header.js
+++ context-practice-1/Header.js
@@ -1,13 +1,16 @@
-import React, {Component} from "react"
+import React, { Component } from "react"
+import UserNameContext from "./userNameContext"
 
 class Header extends Component {
-    render() {
-        return (
-            <header>
-                <p>Welcome, Username!</p>
-            </header>
-        )    
-    }
+  static contextType = UserNameContext
+  render() {
+    const userName = this.context
+    return (
+      <header>
+        <p>Welcome, { userName }!</p>
+      </header>
+    )
+  }
 }
 
 export default Header
