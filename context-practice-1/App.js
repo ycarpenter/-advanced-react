8763f63206000e07921d51d8b5a568e8eacf0784
ycarpenter <yurii.carpenter@gmail.com>
2022-08-31 19:19:47 +0300

    feat: implement userNameContext

--- context-practice-1/App.js
+++ context-practice-1/App.js
@@ -1,16 +1,19 @@
 import React from "react"
-
+import UserNameContext from "./userNameContext"
 import Header from "./Header"
 
 function App() {
-    return (
-        <div>
-            <Header />
-            <main>
-                <p className="main">No new notifications, Username! 🎉</p>
-            </main>
-        </div>
-    )
+  return (
+    <div>
+      <UserNameContext.Consumer>
+        { value => (
+          <main>
+            <p className="main">No new notifications, { value }! 🎉</p>
+          </main>
+        ) }
+      </UserNameContext.Consumer>
+      <Header />
+    </div>
+  )
 }
-
 export default App
